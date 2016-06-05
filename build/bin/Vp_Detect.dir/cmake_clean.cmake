38aab265c3ab2b7686fac3b23e53a9164752a6f9
Tianz <247072146@qq.com>
2016-06-05 16:39:47 +0800

    dev file

---
+++ build/bin/Vp_Detect.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/../bin/Vp_Detect.dir/src/errorNIETO.cpp.o"
+  "CMakeFiles/../bin/Vp_Detect.dir/src/lmmin.cpp.o"
+  "CMakeFiles/../bin/Vp_Detect.dir/src/lsd.cpp.o"
+  "CMakeFiles/../bin/Vp_Detect.dir/src/LSWMS.cpp.o"
+  "CMakeFiles/../bin/Vp_Detect.dir/src/main.cpp.o"
+  "CMakeFiles/../bin/Vp_Detect.dir/src/MSAC.cpp.o"
+  "../bin/Vp_Detect.pdb"
+  "../bin/Vp_Detect"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/../bin/Vp_Detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
